61ce77c8f9a38ff0e7ecb8f93e1721e54b23a683
richard Alford <richie@richards-MacBook.local>
2018-10-25 22:47:03 -0700

    commit

---
+++ WarriorProof/WarriorProof/Mappings/RegistrationViewModelMapper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using WarriorProof.Contracts.Models;
+using WarriorProof.Models;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WarriorProof.Mappings
+{
+    public static class RegistrationViewModelMapper
+    {
+        public static WarriorUser ToWarriorUser(this RegistrationViewModel model)
+        {
+            var warriorUser = new WarriorUser
+            {
+                Password = model.LoginInfo.Password,
+                UserName = model.LoginInfo.UserName,
+                PersonalInfo = new PersonalInfo
+                {
+                    FirstName = model.PersonalInfo.FirstName,
+                    LastName = model.PersonalInfo.LastName,
+                    EmailAddress = model.PersonalInfo.EmailAddress,
+                    PhoneNumber = model.PersonalInfo.PhoneNumber
+                },
+                CompanyInfo = new CompanyInfo
+                {
+                    CompanyName = model.CompanyInfo.CompanyName,
+                    EmailAddress = model.CompanyInfo.EmailAddress,
+                    PhoneNumber = model.CompanyInfo.PhoneNumber,
+                    Address = new Address
+                    {
+                        AddressLine1 = model.CompanyInfo.AddressLine1,
+                        AddressLine2 = model.CompanyInfo.AddressLine2,
+                        City = model.CompanyInfo.City,
+                        State = model.CompanyInfo.State,
+                        ZipCode = model.CompanyInfo.ZipCode
+                    },
+                    Trucks = new Trucks
+                    {
+                        NumberOfTrucks = model.TrucksInfo.NumberOfTrucks,
+                        TrucksLineOfBusinesses = model.TrucksInfo.LineOfBusinesses.ToList()
+                                                      .Select(x => new TrucksLineOfBusiness { LineOfBusiness_Id = Int32.Parse(x) }).ToList()
+               
+                    }
+                }
+            };
+            return warriorUser;
+        }
+
+
+    }
+
+}
